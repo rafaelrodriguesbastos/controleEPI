7726f3c90614c1321ddb626650491da6cdbc1c5e
Rafael Bastos <rafaelrodriguesbastos@gmail.com>
2021-11-30 20:11:57 -0300

    Alterado o setor_editar e setor_salvar para permitir a alteração do registro

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
-    $conexao = mysqli_connect("localhost","usuario","senha", "epi") or 
+    $conexao = mysqli_connect("localhost","root","mysql", "epi") or 
     die("Erro ao conectar ao banco de dados!");
 
 ?>
--- setor_editar.php
+++ setor_editar.php
@@ -1,13 +1,43 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);    
     require_once("session.php");
     require("menu.php");
+    require("conexao.php");
+
+	if (isset($_GET['idsetor']) && $_GET['idsetor'] != '') {
+		$sql = "select
+				nome
+			from
+				setor
+			where
+				idsetor = " . $_GET['idsetor'];
+		$resultado = mysqli_query($conexao, $sql);
+
+		if (mysqli_num_rows($resultado) > 0) {
+			$dados = mysqli_fetch_array($resultado);
+			$idsetor = $_GET['idsetor'];
+			$nome = $dados['nome'];
+		}
+		else {
+			$idsetor = "";
+			$nome = "";
+		}
+	}
+	else {
+		$idsetor = "";
+		$nome = "";
+	}	
+
 
 ?>
 
 <h3>Setor</h3>
 
 <form action="setor_salvar.php" method="POST">
-    <label>Nome</label> <input type="text" name="nome" maxlength="45" size="45">
+	<input type="hidden" name="idsetor" value="<?php echo $idsetor; ?>">
+    <label>Nome</label> <input type="text" name="nome" maxlength="45" size="45" value="<?php echo $nome; ?>">
     <br/>
     <br/>
     <input type="submit" value="Salvar">
